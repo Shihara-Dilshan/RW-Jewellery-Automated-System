6362a1b8550e82e44f6ab57ef1952350b792361b
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-07-09 22:24:53 -0400

    class -> className

--- itp-project/Front-End/src/components/Home/Slider/Slider.jsx
+++ itp-project/Front-End/src/components/Home/Slider/Slider.jsx
@@ -16,40 +16,40 @@ class Slider extends Component {
     });
 
     return (
-      <div class="slider test">
-        <ul class="slides">
+      <div className="slider test">
+        <ul className="slides">
           <li>
             <img alt="" src="https://raw.githubusercontent.com/Shihara-Dilshan/img/master/wp4923981-react-js-wallpapers.jpg" />
-            <div class="caption center-align">
+            <div className="caption center-align">
               <h3>This is our big Tagline!</h3>
-              <h5 class="light grey-text text-lighten-3">
+              <h5 className="light grey-text text-lighten-3">
                 Here's our small slogan.
               </h5>
             </div>
           </li>
           <li>
             <img alt="" src="https://raw.githubusercontent.com/Shihara-Dilshan/img/master/wp4924004-react-js-wallpapers.jpg" />
-            <div class="caption left-align">
+            <div className="caption left-align">
               <h3>Left Aligned Caption</h3>
-              <h5 class="light grey-text text-lighten-3">
+              <h5 className="light grey-text text-lighten-3">
                 Here's our small slogan.
               </h5>
             </div>
           </li>
           <li>
             <img alt="" src="https://raw.githubusercontent.com/Shihara-Dilshan/img/master/wp4923978-react-js-wallpapers.jpg" />
-            <div class="caption right-align">
+            <div className="caption right-align">
               <h3>Right Aligned Caption</h3>
-              <h5 class="light grey-text text-lighten-3">
+              <h5 className="light grey-text text-lighten-3">
                 Here's our small slogan.
               </h5>
             </div>
           </li>
           <li>
             <img alt="" src="https://raw.githubusercontent.com/Shihara-Dilshan/img/master/wp4923978-react-js-wallpapers.jpg" />
-            <div class="caption center-align">
+            <div className="caption center-align">
               <h3>This is our big Tagline!</h3>
-              <h5 class="light grey-text text-lighten-3">
+              <h5 className="light grey-text text-lighten-3">
                 Here's our small slogan.
               </h5>
             </div>

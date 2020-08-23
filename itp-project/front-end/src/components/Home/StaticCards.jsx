03318b41acc12fe00365e662f79ae8411adcd338
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-08-23 17:56:13 +0800

    grey text added

--- itp-project/front-end/src/components/Home/StaticCards.jsx
+++ itp-project/front-end/src/components/Home/StaticCards.jsx
@@ -9,27 +9,27 @@ class StaticCards extends Component {
           <div className="row">
             <div className="col s12 m4">
               <div className="card-panel">
-                <i className="material-icons large teal-text">all_inclusive</i>
+                <i className="material-icons large grey-text">all_inclusive</i>
                 <h4>Valueble items</h4>
                 <p>We always Deliver the best product for you</p>
               </div>
             </div>
             <div className="col s12 m4">
               <div className="card-panel">
-                <i className="material-icons large teal-text">fitness_center</i>
+                <i className="material-icons large grey-text">fitness_center</i>
                 <h4>Fast delivery</h4>
                 <p>Available delivery for inside the any part of the country</p>
               </div>
             </div>
             <div className="col s12 m4">
               <div className="card-panel">
-                <i className="material-icons large teal-text">
+                <i className="material-icons large grey-text">
                   insert_emoticon
                 </i>
                 <h4>Secure Payment</h4>
                 <p>
                   We never collect your personal credentials{" "}
-                  <span className="span teal-text">
+                  <span className="span grey-text">
                     {" "}
                   </span>
                 </p>
